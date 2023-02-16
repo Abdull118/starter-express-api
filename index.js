c2b9431802254eb0c9c403e7229dc5d30b928c1d
Ahmad N <naimi.airsh@gmail.com>
2023-02-16 11:41:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,62 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const bodyParser = require('body-parser')
+const mailjet = require('node-mailjet').apiConnect(
+    'f4a0f153f4d1bf28b57b1fcbe1ead5c1',
+  'b92df3a4e7866ddf9372a1a80b50fa04'
+)
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/send-email', (req, res) => {
+  const { name, email, service, phone, customService, description } = req.body
+  const serviceRequested = service === 'Other' ? customService : service
+  const recipientEmail = process.env.RECIPIENT_EMAIL || 'naimi.airsh@gmail.com'
+
+  const request = mailjet
+    .post('send', { version: 'v3.1' })
+    .request({
+      Messages: [
+        {
+          From: {
+            Email: email,
+            Name: name,
+          },
+          To: [
+            {
+              Email: 'naimi.airsh@gmail.com',
+              Name: 'You',
+            },
+          ],
+          Subject: `Lead: ${serviceRequested}`,
+          TextPart: `Name: ${name}\nEmail: naimi.airsh@gmail.com\nService Requested: ${serviceRequested}\nPhone: ${phone}`,
+          HTMLPart: `
+            <h3>Service Request</h3>
+            <p>Name: ${name}</p>
+            <p>Email: naimi.airsh@gmail.com</p>
+            <p>Service Requested: ${serviceRequested}</p>
+            <p>Description: ${description}
+            <p>Phone: ${phone}</p>
+          `,
+        },
+      ],
+    })
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => {
+      res.status(500).send({ message: 'Error sending email' })
+      console.log(err)
+    })
 })
-app.listen(process.env.PORT || 3000)
+
+const port = process.env.PORT || 4000
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
