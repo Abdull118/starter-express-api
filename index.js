cb8aac40f7b4202ff86b76b5bd62fa60af490b38
Ahmad N <naimi.airsh@gmail.com>
2023-04-25 06:11:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const mailjet = require('node-mailjet').apiConnect(
     'f4a0f153f4d1bf28b57b1fcbe1ead5c1',
   'b92df3a4e7866ddf9372a1a80b50fa04'
 )
-// Add middleware to enable CORS
-app.use((req, res, next) => {
-  res.set('Access-Control-Allow-Origin', '*');
-  res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.set('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+const cors = require('cors');
+
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
+
+app.use(cors());
   
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -51,7 +53,7 @@ app.post('/send-email', (req, res) => {
     })
     .then(res => {
       console.log(res, 'worked')
-        
+        res.status(200).send({ message: 'Email sent successfully' });
     })
     .catch(err => {
       res.status(500).send({ message: 'Error sending email' })
